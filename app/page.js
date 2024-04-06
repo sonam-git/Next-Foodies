955e93c2b9804f81c84c518f885f3722f15cc030
Sonam Sherpa <sherpa.sjs@gmail.com>
2024-04-05 21:30:33 -0700

    add icon and update layout page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
-import Link from 'next/link'
-import styles from "./page.module.css";
-
 export default function Home() {
   return (
-    <main className={styles.main}>
-  <h1>Hello World</h1>
+    <main>
+      <h1 style={{ color: 'white', textAlign: 'center' }}>
+        Time to get started!
+      </h1>
     </main>
   );
 }
